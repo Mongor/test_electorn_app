f2df1d3fc6d7da0701c4d2a1b62931fd59ee106f
Mongor <mitrobaz@gmail.com>
2020-08-25 23:51:38 +0300

    create electron app

--- main.js
+++ main.js
@@ -6,8 +6,33 @@ let win;
 
 const createWindow = () => {
 	win = new BrowserWindow({
-		width: 950,
+		width: 1150,
 		height: 750,
-		icon: __dirname + '',
+		icon: __dirname + '/img/icon_dov.svg',
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
+
+	win.loadFile('index.html');
+
+	win.webContents.openDevTools();
+
+	win.on('closed', () => {
+		win = null;
 	});
 };
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
